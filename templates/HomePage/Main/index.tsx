667ca6edcff4601f26abcf7842c850e99fec2214
Nanda Dias <diasfeh@gmail.com>
2025-03-15 01:47:58 -0300

    feat: atualiza textos da página inicial

--- templates/HomePage/Main/index.tsx
+++ templates/HomePage/Main/index.tsx
@@ -14,11 +14,10 @@ const Main = ({}: MainProps) => {
             <div className="grow px-10 py-20 overflow-y-auto scroll-smooth scrollbar-none 2xl:py-12 md:px-4 md:pt-0 md:pb-6">
                 <div className="mb-10 text-center">
                     <div className="h3 leading-[4rem] 2xl:mb-2 2xl:h4">
-                        Unlock the power of AI
+                        Waves para acelerar seu processo de UX
                     </div>
                     <div className="body1 text-n-4 2xl:body1S">
-                        Chat with the smartest AI - Experience the power of AI
-                        with us
+                        Experimente o poder da IA para deixar o seu trabalho com o que realmente importa: criatividade, empatia e inovação.
                     </div>
                 </div>
                 <Menu className="max-w-[30.75rem] mx-auto" items={navigation} />
