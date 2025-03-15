5e25b3d02cfa166977982ab74b93cb3688440d95
Nanda Dias <diasfeh@gmail.com>
2025-03-15 02:12:56 -0300

    feat: remove chat da página inicial e ajusta layout dos cards

--- templates/HomePage/Main/index.tsx
+++ templates/HomePage/Main/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Message from "@/components/Message";
 import Menu from "@/components/Menu";
 
 import { navigation } from "@/constants/navigation";
@@ -7,26 +6,18 @@ import { navigation } from "@/constants/navigation";
 type MainProps = {};
 
 const Main = ({}: MainProps) => {
-    const [message, setMessage] = useState<string>("");
-
     return (
-        <>
-            <div className="grow px-10 py-20 overflow-y-auto scroll-smooth scrollbar-none 2xl:py-12 md:px-4 md:pt-0 md:pb-6">
-                <div className="mb-10 text-center">
-                    <div className="h3 leading-[4rem] 2xl:mb-2 2xl:h4">
-                        Waves para acelerar seu processo de UX
-                    </div>
-                    <div className="body1 text-n-4 2xl:body1S">
-                        Experimente o poder da IA para deixar o seu trabalho com o que realmente importa: criatividade, empatia e inovação.
-                    </div>
+        <div className="px-10 py-20 overflow-y-auto scroll-smooth scrollbar-none 2xl:py-12 md:px-4 md:pt-0 md:pb-6">
+            <div className="mb-10 text-center">
+                <div className="h3 leading-[4rem] 2xl:mb-2 2xl:h4">
+                    Waves para acelerar seu processo de UX
                 </div>
-                <Menu className="max-w-[30.75rem] mx-auto" items={navigation} />
+                <div className="body1 text-n-4 2xl:body1S">
+                    Experimente o poder da IA para deixar o seu trabalho com o que realmente importa: criatividade, empatia e inovação.
+                </div>
             </div>
-            <Message
-                value={message}
-                onChange={(e: any) => setMessage(e.target.value)}
-            />
-        </>
+            <Menu className="max-w-[80rem] mx-auto" items={navigation} />
+        </div>
     );
 };
 
