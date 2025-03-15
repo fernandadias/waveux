1d65da929deda9fbbb1742d320359694f6e9a41b
Nanda Dias <diasfeh@gmail.com>
2025-03-15 01:14:40 -0300

    feat: atualiza página de login com nova identidade visual

--- templates/SignInPage/index.tsx
+++ templates/SignInPage/index.tsx
@@ -8,24 +8,21 @@ import Form from "./Form";
 const SignInPage = () => {
     return (
         <div className="relative flex min-h-screen min-h-screen-ios lg:p-6 md:px-6 md:pt-16 md:pb-10">
-            <div className="relative shrink-0 w-[40rem] p-20 overflow-hidden 2xl:w-[37.5rem] xl:w-[30rem] xl:p-10 lg:hidden">
+            <div className="relative shrink-0 w-[40rem] p-20 overflow-hidden flex flex-col justify-center items-center 2xl:w-[37.5rem] xl:w-[30rem] xl:p-10 lg:hidden">
                 <div className="max-w-[25.4rem]">
-                    <div className="mb-4 h2 text-n-1">
-                        Unlock the power of AI
-                    </div>
-                    <div className="body1 text-n-3">
-                        Chat with the smartest AI - Experience the power of AI
-                        with us
-                    </div>
-                </div>
-                <div className="absolute top-52 left-5 right-5 h-[50rem] xl:top-24">
                     <Image
-                        className="object-contain"
-                        src="/images/create-pic.png"
-                        fill
-                        sizes="(max-width: 1180px) 50vw, 33vw"
-                        alt=""
+                        className="w-[200px] h-auto mb-8 mx-auto"
+                        src="/images/waveux-big.png"
+                        width={200}
+                        height={200}
+                        alt="WaveUX Logo"
                     />
+                    <div className="mb-4 brand-header text-center">
+                        UX com Super poderes
+                    </div>
+                    <div className="body1 text-n-3 text-center">
+                        Acelere seu processo de design usando IA e tenha tempo para <span className="text-secondary-100">focar no que realmente importa!</span>
+                    </div>
                 </div>
             </div>
             <div className="flex grow my-6 mr-6 p-10 bg-n-1 rounded-[1.25rem] lg:m-0 md:p-0 dark:bg-n-6">
